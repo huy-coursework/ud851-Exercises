e0561cee6ea4c4adef76d07b8c455567f493cd54
Huy Vuong <huy-vuong@users.noreply.github.com>
2016-12-21 08:10:13 -0800

    T04A.03 Send ChildActivity text to display from MainActivity

--- Lesson04a-Starting-New-Activities/T04a.03-Exercise-PassingDataBetweenActivities/app/src/main/java/com/example/android/explicitintent/MainActivity.java
+++ Lesson04a-Starting-New-Activities/T04a.03-Exercise-PassingDataBetweenActivities/app/src/main/java/com/example/android/explicitintent/MainActivity.java
@@ -53,8 +53,7 @@ public class MainActivity extends AppCompatActivity {
              */
             @Override
             public void onClick(View v) {
-                // TODO (1) Retrieve the text from the EditText and store it in a variable
-
+                String textEntered = mNameEntry.getText().toString();
                 /*
                  * Storing the Context in a variable in this case is redundant since we could have
                  * just used "this" or "MainActivity.this" in the method call below. However, we
@@ -71,9 +70,8 @@ public class MainActivity extends AppCompatActivity {
                  * the destinationActivity variable. The constructor for an Intent also requires a
                  * context, which we stored in the variable named "context".
                  */
-                Intent startChildActivityIntent = new Intent(context, destinationActivity);
-
-                // TODO (2) Use the putExtra method to put the String from the EditText in the Intent
+                Intent startChildActivityIntent = new Intent(context, destinationActivity)
+                        .putExtra(Intent.EXTRA_TEXT, textEntered);
 
                 /*
                  * Once the Intent has been created, we can use Activity's method, "startActivity"
