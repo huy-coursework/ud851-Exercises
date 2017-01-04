d286ac0260bbb12349a5182c846cfcfb7a2c6703
Huy Vuong <huy-vuong@users.noreply.github.com>
2017-01-03 22:46:22 -0800

    T06.10 Implement validation via OnPreferenceListener for size scaling

--- Lesson06-Visualizer-Preferences/T06.10-Exercise-EditTextPreferenceConstraints/app/src/main/java/android/example/com/visualizerpreferences/SettingsFragment.java
+++ Lesson06-Visualizer-Preferences/T06.10-Exercise-EditTextPreferenceConstraints/app/src/main/java/android/example/com/visualizerpreferences/SettingsFragment.java
@@ -27,9 +27,8 @@ import android.support.v7.preference.PreferenceFragmentCompat;
 import android.support.v7.preference.PreferenceScreen;
 import android.widget.Toast;
 
-// TODO (1) Implement OnPreferenceChangeListener
 public class SettingsFragment extends PreferenceFragmentCompat implements
-        OnSharedPreferenceChangeListener {
+        OnSharedPreferenceChangeListener, Preference.OnPreferenceChangeListener {
 
     @Override
     public void onCreatePreferences(Bundle bundle, String s) {
@@ -50,8 +49,10 @@ public class SettingsFragment extends PreferenceFragmentCompat implements
                 String value = sharedPreferences.getString(p.getKey(), "");
                 setPreferenceSummary(p, value);
             }
+            if (p instanceof EditTextPreference) {
+                p.setOnPreferenceChangeListener(this);
+            }
         }
-        // TODO (3) Add the OnPreferenceChangeListener specifically to the EditTextPreference
     }
 
     @Override
@@ -88,10 +89,33 @@ public class SettingsFragment extends PreferenceFragmentCompat implements
         }
     }
 
-    // TODO (2) Override onPreferenceChange. This method should try to convert the new preference value
-    // to a float; if it cannot, show a helpful error message and return false. If it can be converted
-    // to a float check that that float is between 0 (exclusive) and 3 (inclusive). If it isn't, show
-    // an error message and return false. If it is a valid number, return true.
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (!preference.getKey().equals(getString(R.string.pref_size_key))) {
+            return true;
+        }
+
+        Toast error = Toast.makeText(
+                getContext(),
+                "Please select a number between 0.1 and 3.0",
+                Toast.LENGTH_SHORT);
+
+        String newSizeString = ((String) newValue).trim();
+        if (newSizeString.isEmpty()) {
+            newSizeString = "1";
+        }
+        try {
+            float newSizeValue = Float.parseFloat(newSizeString);
+            if (newSizeValue < 0.1 || newSizeValue > 3.0) {
+                error.show();
+                return false;
+            }
+        } catch (NumberFormatException e) {
+            error.show();
+            return false;
+        }
+        return true;
+    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
