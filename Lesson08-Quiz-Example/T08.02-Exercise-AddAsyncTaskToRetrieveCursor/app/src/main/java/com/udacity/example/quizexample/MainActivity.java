7d08d17f049c5fc952266e60a5b98b8222e95cc3
Huy Vuong <huy-vuong@users.noreply.github.com>
2017-01-05 19:26:13 -0800

    T08.02 Create AsyncTask to query data from terms content provider

--- Lesson08-Quiz-Example/T08.02-Exercise-AddAsyncTaskToRetrieveCursor/app/src/main/java/com/udacity/example/quizexample/MainActivity.java
+++ Lesson08-Quiz-Example/T08.02-Exercise-AddAsyncTaskToRetrieveCursor/app/src/main/java/com/udacity/example/quizexample/MainActivity.java
@@ -16,10 +16,15 @@
 
 package com.udacity.example.quizexample;
 
+import android.content.ContentResolver;
+import android.database.Cursor;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+
+import com.udacity.example.droidtermsprovider.DroidTermsExampleContract;
 
 /**
  * Gets the data from the ContentProvider and shows a series of flash cards.
@@ -30,8 +35,9 @@ public class MainActivity extends AppCompatActivity {
     // The current state of the app
     private int mCurrentState;
 
-    // TODO (3) Create an instance variable storing a Cursor called mData
     private Button mButton;
+
+    private Cursor mData;
 
     // This state is when the word definition is hidden and clicking the button will therefore
     // show the definition
@@ -50,7 +56,8 @@ public class MainActivity extends AppCompatActivity {
         // Get the views
         mButton = (Button) findViewById(R.id.button_next);
 
-        // TODO (5) Create and execute your AsyncTask here
+        // Create and execute GetTermsTask here
+        new GetTermsTask().execute();
     }
 
     /**
@@ -90,9 +97,24 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    // TODO (1) Create AsyncTask with the following generic types <Void, Void, Cursor>
-    // TODO (2) In the doInBackground method, write the code to access the DroidTermsExample
-    // provider and return the Cursor object
-    // TODO (4) In the onPostExecute method, store the Cursor object in mData
+    public class GetTermsTask extends AsyncTask<Void, Void, Cursor> {
+        @Override
+        protected Cursor doInBackground(Void... noParams) {
+            // Obtain the device's content resolver to make calls against a given URI, which are
+            // then forwarded to the correct content provider to actually process.
+            ContentResolver contentResolver = getContentResolver();
+            return contentResolver.query(
+                    DroidTermsExampleContract.CONTENT_URI,
+                    null,
+                    null,
+                    null,
+                    null);
+        }
+
+        @Override
+        protected void onPostExecute(Cursor termsCursor) {
+            mData = termsCursor;
+        }
+    }
 
 }
