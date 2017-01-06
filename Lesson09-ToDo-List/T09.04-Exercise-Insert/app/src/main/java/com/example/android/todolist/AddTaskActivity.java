991c2da9a2b895b59f63ea26beb97dd6030b319d
Huy Vuong <huy-vuong@users.noreply.github.com>
2017-01-06 03:30:56 -0800

    T09.04 Insert with content resolver in the AddTaskActivity

--- Lesson09-ToDo-List/T09.04-Exercise-Insert/app/src/main/java/com/example/android/todolist/AddTaskActivity.java
+++ Lesson09-ToDo-List/T09.04-Exercise-Insert/app/src/main/java/com/example/android/todolist/AddTaskActivity.java
@@ -16,16 +16,24 @@
 
 package com.example.android.todolist;
 
+import android.content.ContentValues;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.RadioButton;
+import android.widget.Toast;
+
+import com.example.android.todolist.data.TaskContract;
 
 
 public class AddTaskActivity extends AppCompatActivity {
 
     // Declare a member variable to keep track of a task's selected mPriority
     private int mPriority;
+
+    private EditText taskDescriptionEditText;
 
 
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +43,7 @@ public class AddTaskActivity extends AppCompatActivity {
         // Initialize to highest mPriority by default (mPriority = 1)
         ((RadioButton) findViewById(R.id.radButton1)).setChecked(true);
         mPriority = 1;
+        taskDescriptionEditText = (EditText) findViewById(R.id.editTextTaskDescription);
     }
 
 
@@ -43,14 +52,25 @@ public class AddTaskActivity extends AppCompatActivity {
      * It retrieves user input and inserts that new task data into the underlying database.
      */
     public void onClickAddTask(View view) {
-        // Not yet implemented
-        // TODO (6) Check if EditText is empty, if not retrieve input and store it in a ContentValues object
+        // Check if EditText is empty, if not retrieve input and store it in a ContentValues object
+        String taskDescription = taskDescriptionEditText.getText().toString();
+        if (taskDescription.isEmpty()) {
+            return;
+        }
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(TaskContract.TaskEntry.COLUMN_DESCRIPTION, taskDescription);
+        contentValues.put(TaskContract.TaskEntry.COLUMN_PRIORITY, mPriority);
 
-        // TODO (7) Insert new task data via a ContentResolver
+        // Insert new task data via a ContentResolver
+        Uri returnedUri =
+                getContentResolver().insert(TaskContract.TaskEntry.CONTENT_URI, contentValues);
 
-        // TODO (8) Display the URI that's returned with a Toast
+        // Display the URI that's returned with a Toast
         // [Hint] Don't forget to call finish() to return to MainActivity after this insert is complete
-
+        if (returnedUri != null) {
+            Toast.makeText(this, returnedUri.toString(), Toast.LENGTH_SHORT).show();
+        }
+        finish();
     }
 
 
--- Lesson09-ToDo-List/T09.04-Exercise-Insert/app/src/main/java/com/example/android/todolist/data/TaskContentProvider.java
+++ Lesson09-ToDo-List/T09.04-Exercise-Insert/app/src/main/java/com/example/android/todolist/data/TaskContentProvider.java
@@ -17,10 +17,13 @@
 package com.example.android.todolist.data;
 
 import android.content.ContentProvider;
+import android.content.ContentUris;
 import android.content.ContentValues;
 import android.content.Context;
 import android.content.UriMatcher;
 import android.database.Cursor;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
 import android.net.Uri;
 import android.support.annotation.NonNull;
 
@@ -78,16 +81,26 @@ public class TaskContentProvider extends ContentProvider {
 
     @Override
     public Uri insert(@NonNull Uri uri, ContentValues values) {
-        // TODO (1) Get access to the task database (to write new data to)
+        // Get access to the task database (to write new data to)
+        SQLiteDatabase db = mTaskDbHelper.getWritableDatabase();
 
-        // TODO (2) Write URI matching code to identify the match for the tasks directory
+        // Write URI matching code to identify the match for the tasks directory
+        if (sUriMatcher.match(uri) != TASKS) {
+            throw new UnsupportedOperationException("Unknown URI: " + uri);
+        }
 
-        // TODO (3) Insert new values into the database
-        // TODO (4) Set the value for the returnedUri and write the default case for unknown URI's
+        // Insert new values into the database
+        long id = db.insert(TaskContract.TaskEntry.TABLE_NAME, null, values);
 
-        // TODO (5) Notify the resolver if the uri has been changed, and return the newly inserted URI
-
-        throw new UnsupportedOperationException("Not yet implemented");
+        // Set the value for the returnedUri and write the default case for unknown URI's
+        if (id < 0) {
+            throw new SQLException("Failed to insert row into: " + uri);
+        } else {
+            Uri returnedUri = ContentUris.withAppendedId(TaskContract.TaskEntry.CONTENT_URI, id);
+            // Notify the resolver if the uri has been changed, and return the newly inserted URI
+            getContext().getContentResolver().notifyChange(returnedUri, null);
+            return returnedUri;
+        }
     }
 
 
