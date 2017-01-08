e4fbf9d5a6906f2cafec19619d2a227bbb3b4ea9
Huy Vuong <huy-vuong@users.noreply.github.com>
2017-01-08 05:16:01 -0800

    T10.03 Add actions to water reminder notification

--- Lesson10-Hydration-Reminder/T10.03-Exercise-NotificationActions/app/src/main/java/com/example/android/background/sync/ReminderTasks.java
+++ Lesson10-Hydration-Reminder/T10.03-Exercise-NotificationActions/app/src/main/java/com/example/android/background/sync/ReminderTasks.java
@@ -17,22 +17,29 @@ package com.example.android.background.sync;
 
 import android.content.Context;
 
+import com.example.android.background.utilities.NotificationUtils;
 import com.example.android.background.utilities.PreferenceUtilities;
 
 public class ReminderTasks {
 
     public static final String ACTION_INCREMENT_WATER_COUNT = "increment-water-count";
-    //  TODO (2) Add a public static constant called ACTION_DISMISS_NOTIFICATION
+    public static final String ACTION_DISMISS_NOTIFICATION = "dismiss-notification";
 
     public static void executeTask(Context context, String action) {
-        if (ACTION_INCREMENT_WATER_COUNT.equals(action)) {
-            incrementWaterCount(context);
+        switch (action) {
+            case ACTION_INCREMENT_WATER_COUNT:
+                incrementWaterCount(context);
+                break;
+            case ACTION_DISMISS_NOTIFICATION:
+                NotificationUtils.clearNotifications(context);
+                break;
+            default:
+                throw new UnsupportedOperationException("Unknown action: " + action);
         }
-        //      TODO (3) If the user ignored the reminder, clear the notification
     }
 
     private static void incrementWaterCount(Context context) {
         PreferenceUtilities.incrementWaterCount(context);
-        //      TODO (4) If the water count was incremented, clear any notifications
+        NotificationUtils.clearNotifications(context);
     }
 }
